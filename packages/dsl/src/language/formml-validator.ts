427c550386d345da87b44b566e68909f08386e12
Jindong Zhang <jindong.zhannng@gmail.com>
2023-07-17 15:26:25 +0800

    feat(dsl): initiate dsl package

---
+++ packages/dsl/src/language/formml-validator.ts
@@ -0,0 +1,35 @@
+import { ValidationAcceptor, ValidationChecks } from 'langium'
+import { FormMLAstType, Person } from './generated/ast'
+import type { FormMLServices } from './formml-module'
+
+/**
+ * Register custom validation checks.
+ */
+export function registerValidationChecks(services: FormMLServices) {
+  const registry = services.validation.ValidationRegistry
+  const validator = services.validation.FormMLValidator
+  const checks: ValidationChecks<FormMLAstType> = {
+    Person: validator.checkPersonStartsWithCapital,
+  }
+  registry.register(checks, validator)
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class FormMLValidator {
+  checkPersonStartsWithCapital(
+    person: Person,
+    accept: ValidationAcceptor
+  ): void {
+    if (person.name) {
+      const firstChar = person.name.substring(0, 1)
+      if (firstChar.toUpperCase() !== firstChar) {
+        accept('warning', 'Person name should start with a capital.', {
+          node: person,
+          property: 'name',
+        })
+      }
+    }
+  }
+}
--- packages/dsl/src/lib/dsl.spec.ts
+++
@@ -1,7 +0,0 @@
-import { dsl } from './dsl'
-
-describe('dsl', () => {
-  test('should work', () => {
-    expect(dsl()).toEqual('dsl')
-  })
-})
