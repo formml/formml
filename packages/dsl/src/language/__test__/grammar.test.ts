f8889ad94da07a9f5e79c402f6978fcad4bbe3b7
Jindong Zhang <jindong.zhannng@gmail.com>
2023-12-22 20:38:19 +0800

    test(dsl): add simple fields test

---
+++ packages/dsl/src/language/__test__/grammar.test.ts
@@ -0,0 +1,26 @@
+import { parseHelper } from 'langium/test'
+import { createInMemoryServices } from '../formml-module.js'
+import { FormML } from '../generated/ast.js'
+
+describe('grammar', () => {
+  const services = createInMemoryServices()
+  const serialize = (ast: FormML) =>
+    services.FormML.serializer.JsonSerializer.serialize(ast, { space: 2 })
+  const parser = parseHelper<FormML>(services.FormML)
+
+  describe('simple fields', () => {
+    test('should support primitives', async () => {
+      const content = `
+        form ExampleForm {
+          Number   numberField
+          Currency currencyField
+          Text     textField
+          Boolean	 booleanField
+          Date		 dateField
+        }
+      `
+      const ast = (await parser(content)).parseResult.value
+      expect(serialize(ast)).toMatchSnapshot()
+    })
+  })
+})
