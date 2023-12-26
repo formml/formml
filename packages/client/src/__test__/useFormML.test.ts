a1f81222e24d5ee00eac9c3553b1f8b4c6a4dbf2
Jindong Zhang <jindong.zhannng@gmail.com>
2023-12-26 16:13:35 +0800

    feat(client): prevent default submit behavior

--- packages/client/src/__test__/useFormML.test.ts
+++ packages/client/src/__test__/useFormML.test.ts
@@ -3,6 +3,10 @@ import useFormML from '../useFormML.js'
 
 describe('useFormML', () => {
   describe('handleSubmit', () => {
+    const dummyEvent = new SubmitEvent(
+      'submit',
+    ) as unknown as React.FormEvent<HTMLFormElement>
+
     test('should be a function', () => {
       // Arrange
       const dummyDsl = `abc`
@@ -22,11 +26,25 @@ describe('useFormML', () => {
       // Act
       const { result } = renderHook(() => useFormML(dummyDsl))
       const eventHandler = result.current.handleSubmit(onSubmit)
-      eventHandler()
+      eventHandler(dummyEvent)
 
       // Assert
       const expectedData = {}
       expect(onSubmit).toBeCalledWith(expectedData)
+    })
+
+    test('should prevent default submit behavior', () => {
+      // Arrange
+      const dummyDsl = `abc`
+      const preventDefault = vi.spyOn(dummyEvent, 'preventDefault')
+
+      // Act
+      const { result } = renderHook(() => useFormML(dummyDsl))
+      const eventHandler = result.current.handleSubmit(() => {})
+      eventHandler(dummyEvent)
+
+      // Assert
+      expect(preventDefault).toBeCalled()
     })
   })
 })
--- packages/client/src/useFormML.ts
+++ packages/client/src/useFormML.ts
@@ -1,10 +1,16 @@
+import React from 'react'
+
 export type OnSubmitCallback = (data: object) => void
 
 export default function useFormML(_dsl: string) {
   const data = {}
+  const handleSubmit: (
+    onSubmit: OnSubmitCallback,
+  ) => React.FormEventHandler<HTMLFormElement> = (onSubmit) => (event) => {
+    event.preventDefault()
+    onSubmit(data)
+  }
   return {
-    handleSubmit: (onSubmit: OnSubmitCallback) => () => {
-      onSubmit(data)
-    },
+    handleSubmit,
   }
 }
