aedf76804c6f6f4b96ce2c50f473dd4a4c8de9a7
Jindong Zhang <jindong.zhannng@gmail.com>
2024-04-26 15:56:59 +0800

    feat(client): validate with valibot

--- packages/client/src/validate/Validator.ts
+++ packages/client/src/validate/Validator.ts
@@ -1,11 +1,16 @@
 import { Field } from '@formml/dsl'
+import { BaseSchema, number, safeParse } from 'valibot'
 
 import * as JsType from '../JsTypes.js'
 
 export default class Validator {
-  constructor(_schema: Field) {}
+  private readonly _valibotSchema: BaseSchema
+  constructor(_schema: Field) {
+    this._valibotSchema = number()
+  }
 
-  validate(_value: JsType.PrimitiveType) {
-    return { errors: [], isValid: true }
+  validate(value: JsType.PrimitiveType) {
+    const result = safeParse(this._valibotSchema, value)
+    return { errors: result.issues, isValid: result.success }
   }
 }
--- packages/client/src/validate/__test__/Validator.test.ts
+++ packages/client/src/validate/__test__/Validator.test.ts
@@ -25,6 +25,35 @@ describe('Validator', () => {
 
     // Assert
     expect(result.isValid).toBe(true)
-    expect(result.errors).toHaveLength(0)
+    expect(result.errors).toBeUndefined()
+  })
+
+  test('should invalidate a field if it is not aligned with schema', () => {
+    // Arrange
+    const schema: Field = {
+      $container: {} as Form,
+      $type: 'Field',
+      annotations: [
+        {
+          $container: {} as Field,
+          $type: 'Annotation',
+          name: 'required',
+        },
+      ],
+      name: 'numberField',
+      type: 'num',
+    }
+    const validator = new Validator(schema)
+
+    // Act
+    const result = validator.validate(undefined)
+
+    // Assert
+    expect(result.isValid).toBe(false)
+    expect(result.errors).toEqual([
+      expect.objectContaining({
+        message: expect.any(String),
+      }),
+    ])
   })
 })
