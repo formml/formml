f2876ae40390031cbb105dde75468c89e72768e5
Jindong Zhang <jindong.zhannng@gmail.com>
2024-05-06 16:40:50 +0800

    feat(client): invalidate non-string input

--- packages/client/src/validate/valibot/__test__/buildSchema.test.ts
+++ packages/client/src/validate/valibot/__test__/buildSchema.test.ts
@@ -23,6 +23,42 @@ describe('buildSchema', () => {
         // Assert
         expect(result.success).toBe(true)
       })
+
+      test('should invalidate non-string input if field type is text', () => {
+        // Arrange
+        const field: Field = {
+          $container: {} as Form,
+          $type: 'Field',
+          annotations: [],
+          name: 'field',
+          type: 'text',
+        }
+
+        // Act
+        const schema = buildSchema(field)
+        const result = v.safeParse(schema, 123)
+
+        // Assert
+        expect(result.success).toBe(false)
+        expect(result.issues).toMatchInlineSnapshot(`
+          [
+            {
+              "abortEarly": undefined,
+              "abortPipeEarly": undefined,
+              "context": "string",
+              "expected": "string",
+              "input": 123,
+              "issues": undefined,
+              "lang": undefined,
+              "message": "Invalid type: Expected string but received 123",
+              "path": undefined,
+              "reason": "type",
+              "received": "123",
+              "skipPipe": undefined,
+            },
+          ]
+        `)
+      })
     })
   })
 })
