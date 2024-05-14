be341de64ee247a496aa5c7e79759e6b262147d5
Jindong Zhang <jindong.zhannng@gmail.com>
2024-05-14 20:54:31 +0800

    test(client): execution order

--- packages/client/src/decorators/__test__/validate.test.ts
+++ packages/client/src/decorators/__test__/validate.test.ts
@@ -22,6 +22,27 @@ describe('validate', () => {
       expect(mockedValidate).toBeCalledWith(dummyIndex)
     })
 
+    test('should do validation after execution of original method', () => {
+      // Arrange
+      const executions: string[] = []
+      const mockedValidate = vi.fn(() => executions.push('validate'))
+      const mockedOriginalMethod = vi.fn(() => executions.push('original'))
+      const form = {
+        configs: { validateOn: { initial: 'change' } },
+        validate: mockedValidate,
+      } as unknown as FormML
+      const dummyIndex = {}
+      const dummyContext = {} as ClassMethodDecoratorContext
+
+      // Act
+      const decorator = validate({ eventName: 'change' })
+      const decoratedMethod = decorator(mockedOriginalMethod, dummyContext)
+      decoratedMethod.call(form, dummyIndex)
+
+      // Assert
+      expect(executions).toEqual(['original', 'validate'])
+    })
+
     test('should not do validation if given event name do not match configured event', () => {
       // Arrange
       const mockedValidate = vi.fn()
