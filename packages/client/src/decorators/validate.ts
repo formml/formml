5260ceae7a0508f35a564c1ea29c39118333897c
Jindong Zhang <jindong.zhannng@gmail.com>
2024-05-14 21:45:05 +0800

    refactor(client): refine types

--- packages/client/src/decorators/validate.ts
+++ packages/client/src/decorators/validate.ts
@@ -1,21 +1,17 @@
 import { FormML, FormMLEvent } from '../FormML.js'
 
 export default function validate({ eventName }: { eventName: FormMLEvent }) {
-  return function decorator(
-    originalMethod: (
-      this: FormML,
-      index: object,
-      ..._args: unknown[]
-    ) => unknown,
+  return function decorator<TArgs extends unknown[], TReturn>(
+    originalMethod: (this: FormML, index: object, ..._args: TArgs) => TReturn,
     _context: ClassMethodDecoratorContext<
       FormML,
-      (this: FormML, index: object, ..._args: unknown[]) => unknown
+      (this: FormML, index: object, ..._args: TArgs) => TReturn
     >,
   ) {
     return function decoratedMethod(
       this: FormML,
       index: object,
-      ..._args: unknown[]
+      ..._args: TArgs
     ) {
       const result = originalMethod.call(this, index, ..._args)
       if (this.configs.validateOn.initial === eventName) {
