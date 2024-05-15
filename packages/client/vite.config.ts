2eb36730ebc26932a7a4b78ba21e5260305e2d8f
Jindong Zhang <jindong.zhannng@gmail.com>
2024-05-15 17:51:43 +0800

    build(client): support js decorators when running tests

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,9 +1,27 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
+import ts from 'typescript'
 import { defineConfig } from 'vite'
+import { vitePluginTypescriptTransform } from 'vite-plugin-typescript-transform'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePluginTypescriptTransform({
+      enforce: 'pre',
+      filter: {
+        files: {
+          include: '**/FormML.ts',
+        },
+      },
+      tsconfig: {
+        override: {
+          module: ts.ModuleKind.ESNext,
+          target: ts.ScriptTarget.ES2022,
+        },
+      },
+    }),
+  ],
 
   test: {
     environment: 'jsdom',
