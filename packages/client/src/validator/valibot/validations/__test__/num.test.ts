024131f00276851378431ecec8d89430d7f59683
Jindong Zhang <jindong.zhannng@gmail.com>
2024-05-23 18:11:44 +0800

    test(client): add num test cases

--- packages/client/src/validator/valibot/validations/__test__/num.test.ts
+++ packages/client/src/validator/valibot/validations/__test__/num.test.ts
@@ -14,6 +14,20 @@ describe('num', () => {
     expect(result.success).toBe(true)
   })
 
+  test.each(['Infinity', '-Infinity'])(
+    'should validate infinities - "%s"',
+    (input) => {
+      // Arrange
+      const schema = v.pipe(v.string(), num())
+
+      // Act
+      const result = v.safeParse(schema, input)
+
+      // Assert
+      expect(result.success).toBe(true)
+    },
+  )
+
   test.each(['', '  ', ' \n\t'])('should validate a blank string', (input) => {
     // Arrange
     const schema = v.pipe(v.string(), num())
@@ -47,6 +61,36 @@ describe('num', () => {
           "message": "Invalid input: Expected numerical but received "abc"",
           "path": undefined,
           "received": ""abc"",
+          "requirement": [Function],
+          "skipPipe": undefined,
+          "type": "num",
+        },
+      ]
+    `)
+  })
+
+  test('should invalidate NaN', () => {
+    // Arrange
+    const schema = v.pipe(v.string(), num())
+
+    // Act
+    const result = v.safeParse(schema, 'NaN')
+
+    // Assert
+    expect(result.success).toBe(false)
+    expect(result.issues).toMatchInlineSnapshot(`
+      [
+        {
+          "abortEarly": undefined,
+          "abortPipeEarly": undefined,
+          "expected": "numerical",
+          "input": "NaN",
+          "issues": undefined,
+          "kind": "validation",
+          "lang": undefined,
+          "message": "Invalid input: Expected numerical but received "NaN"",
+          "path": undefined,
+          "received": ""NaN"",
           "requirement": [Function],
           "skipPipe": undefined,
           "type": "num",
