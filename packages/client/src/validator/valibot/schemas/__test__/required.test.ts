684ecc002768e0c72680f6af7a78f2f00906b875
Jindong Zhang <jindong.zhannng@gmail.com>
2024-05-30 17:20:04 +0800

    feat(client): forward input to wrapped schema

--- packages/client/src/validator/valibot/schemas/__test__/required.test.ts
+++ packages/client/src/validator/valibot/schemas/__test__/required.test.ts
@@ -13,4 +13,34 @@ describe('required', () => {
     // Assert
     expect(result.success).toBe(true)
   })
+
+  test('should forward input to wrapped schema if validation passes', () => {
+    // Arrange
+    const schema = required(v.string())
+
+    // Act
+    const result = v.safeParse(schema, 123)
+
+    // Assert
+    expect(result.success).toBe(false)
+    expect(result.issues).toMatchInlineSnapshot(`
+      [
+        {
+          "abortEarly": undefined,
+          "abortPipeEarly": undefined,
+          "expected": "string",
+          "input": 123,
+          "issues": undefined,
+          "kind": "schema",
+          "lang": undefined,
+          "message": "Invalid type: Expected string but received 123",
+          "path": undefined,
+          "received": "123",
+          "requirement": undefined,
+          "skipPipe": undefined,
+          "type": "string",
+        },
+      ]
+    `)
+  })
 })
