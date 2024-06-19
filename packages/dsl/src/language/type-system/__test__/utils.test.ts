88d11440051dfe19aae3fc0cd0e1c7b6fce7315d
Jindong Zhang <jindong.zhannng@gmail.com>
2024-06-19 15:01:29 +0800

    test(dsl): add object type assignment check

--- packages/dsl/src/language/type-system/__test__/utils.test.ts
+++ packages/dsl/src/language/type-system/__test__/utils.test.ts
@@ -3,33 +3,54 @@ import { isAssignable, stringify } from '../utils.js'
 
 describe('utils', () => {
   describe('isAssignable', () => {
-    test.each([t.Text, t.Num, t.Bool, t.Datetime, t.Decimal])(
-      'should always return true if source is any',
-      (target) => {
-        expect(isAssignable(t.Any, target)).toBe(true)
-      },
-    )
+    test.each([
+      t.Text,
+      t.Num,
+      t.Bool,
+      t.Datetime,
+      t.Decimal,
+      t.createObjectType({ a: t.Text }),
+    ])('should always return true if source is any', (target) => {
+      expect(isAssignable(t.Any, target)).toBe(true)
+    })
 
-    test.each([t.Text, t.Num, t.Bool, t.Datetime, t.Decimal])(
-      'should always return true if target is any',
-      (source) => {
-        expect(isAssignable(source, t.Any)).toBe(true)
-      },
-    )
+    test.each([
+      t.Text,
+      t.Num,
+      t.Bool,
+      t.Datetime,
+      t.Decimal,
+      t.createObjectType({ a: t.Text }),
+    ])('should always return true if target is any', (source) => {
+      expect(isAssignable(source, t.Any)).toBe(true)
+    })
 
-    test.each([t.Text, t.Num, t.Bool, t.Datetime, t.Decimal])(
-      'should return true if source is equal to target',
-      (type) => {
-        expect(isAssignable(type, type)).toBe(true)
-      },
-    )
+    test.each([
+      t.Text,
+      t.Num,
+      t.Bool,
+      t.Datetime,
+      t.Decimal,
+      t.createObjectType({ a: t.Text }),
+    ])('should return true if source is equal to target', (type) => {
+      expect(isAssignable(type, type)).toBe(true)
+    })
 
     const permutations = <T>(items: T[]): (readonly [T, T])[] =>
       items.flatMap((x) =>
         items.filter((y) => x !== y).map((y) => [x, y] as const),
       )
 
-    test.each(permutations([t.Text, t.Num, t.Bool, t.Datetime, t.Decimal]))(
+    test.each(
+      permutations([
+        t.Text,
+        t.Num,
+        t.Bool,
+        t.Datetime,
+        t.Decimal,
+        t.createObjectType({ a: t.Text }),
+      ]),
+    )(
       'should return false if source and target are not in same category - %s vs %s',
       (source, target) => {
         expect(isAssignable(source, target)).toBe(false)
