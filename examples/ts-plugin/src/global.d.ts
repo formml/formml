2b4267125dda1bddb3f2d3cf01a259a832d6532a
Jindong Zhang <jindong.zhannng@gmail.com>
2024-07-10 15:01:34 +0800

    feat(ts-plugin-example): update example

--- examples/ts-plugin/src/global.d.ts
+++
@@ -1,6 +0,0 @@
-// Default type for formml files
-declare module '*.formml' {
-  export type FormData = string;
-  const value: FormData;
-  export default value;
-}
--- examples/ts-plugin/src/index.ts
+++ examples/ts-plugin/src/index.ts
@@ -1,7 +1,3 @@
 import data from './data.formml' with { type: 'formml' }
 
-// will not support below
-const typeNode: import('./data.formml', { with: { type: 'formml' }}).FormData = 'data'
-const asyncData = import('./data.formml', { with: { type: 'formml' } })
-
-export { data, typeNode, asyncData }
+const form = data.form
