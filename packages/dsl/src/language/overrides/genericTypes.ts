9fd5336db78b8daadaaeede1af893764e622ef53
Jindong Zhang <jindong.zhannng@gmail.com>
2024-07-24 12:22:14 +0800

    refactor(dsl): use generic interfaces

--- packages/dsl/src/language/overrides/genericTypes.ts
+++ packages/dsl/src/language/overrides/genericTypes.ts
@@ -1,21 +1,22 @@
 import * as ast from '../generated/ast.js'
 
-export type FormMLSchema<TForm extends Form = Form> = ast.FormMLSchema & {
+export interface FormMLSchema<TForm extends Form = Form>
+  extends ast.FormMLSchema {
   form: TForm
 }
 
-export type Form<
+export interface Form<
   TName extends string = string,
-  TFields extends readonly Field[] = Array<Field>,
-> = ast.Form & {
+  TFields extends Field[] = Field[],
+> extends ast.Form {
   fields: TFields
   name: TName
 }
 
-export type Field<
+export interface Field<
   TName extends string = string,
   TType extends ast.PRIMITIVE = ast.PRIMITIVE,
-> = ast.Field & {
+> extends ast.Field {
   name: TName
   type: TType
 }
