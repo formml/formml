53bded0f098c7705bbf465057f85ebd77f7b62b6
Jindong Zhang <jindong.zhannng@gmail.com>
2024-10-05 17:18:02 +0800

    feat(core): init package

---
+++ packages/core/README.md
@@ -0,0 +1,1 @@
+# `@formml/core`
---
+++ packages/core/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  clean: true,
+  dts: true,
+  entry: ['src/index.ts'],
+  // noExternal: ['@formml/dsl'],
+  outDir: './dist/cjs',
+  sourcemap: true,
+})
