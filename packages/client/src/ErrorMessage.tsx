8605075d8ee3bcf5ad366cb0b51f65ae380568dc
Jindong Zhang <jindong.zhannng@gmail.com>
2024-11-27 17:19:00 +0800

    docs(client): refine props jsdoc

--- packages/client/src/ErrorMessage.tsx
+++ packages/client/src/ErrorMessage.tsx
@@ -21,30 +21,41 @@ function ErrorMessageImpl(
   return React.createElement(as, { ...props, ref }, meta.error.message)
 }
 
-type ErrorMessageComponent = {
-  (props: { $bind: BaseIndex }): React.ReactNode
+interface BaseErrorMessageProps {
+  /** The field index to bind to */
+  $bind: BaseIndex
+}
+
+interface HTMLElementAsErrorMessageProps<
+  TElementName extends keyof React.ReactHTML,
+> extends BaseErrorMessageProps {
+  /** HTML element name to render the error message with */
+  as: TElementName
+}
+
+interface ErrorMessageComponent {
+  (props: BaseErrorMessageProps): React.ReactNode
   <TElementName extends keyof React.ReactHTML>(
-    props: {
-      $bind: BaseIndex
-      as: TElementName
-    } & React.ComponentPropsWithRef<TElementName>,
+    props: HTMLElementAsErrorMessageProps<TElementName> &
+      React.ComponentPropsWithRef<TElementName>,
   ): React.ReactNode
 }
 
 /**
  * A React component that displays validation error messages for bound form field
  * @param props - Component props
- * @param props.$bind - The field index to bind to
- * @param props.as - Optional HTML element name to render the error message with
  * @returns React element showing error message if validation fails
  * @example
- * ```tsx
  * // Returns string directly if no `as` prop was given
  * <span><ErrorMessage $bind={$form.email} /></span>
  *
  * // Returns div, accepting any other props that div can take
- * <ErrorMessage $bind={$form.email} as="div" className="error" />
- * ```
+ * <ErrorMessage
+ *   $bind={$form.email}
+ *   as="div"
+ *   className="error"
+ *   ref={myRef}
+ * />
  */
 export const ErrorMessage: ErrorMessageComponent =
   React.forwardRef(ErrorMessageImpl)
